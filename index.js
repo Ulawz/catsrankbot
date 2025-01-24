d3d97fa7a1f8b73454a619b2ad9b5905f7f93495
Ulawz <catsofficialchannel@gmail.com>
2025-01-23 21:58:27 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const rbx = require("noblox.js");
+const app = express();
+
+// Using environment variables
+var groupId = process.env.GROUP_ID;
+var cookie = process.env.COOKIE;
+
+app.use(express.static("public"));
+
+async function startApp() {
+  await rbx.setCookie(cookie);
+  let currentUser = await rbx.getAuthenticatedUser();
+  console.log(currentUser.name);
+}
+startApp();
+
+// /ranker endpoint with updated query parameter handling
+app.get("/ranker", async (req, res) => {
+  var user = req.query.userid;
+  var rank = req.query.rank;
+
+  try {
+    await rbx.setRank(groupId, parseInt(user), parseInt(rank));
+    res.json("Ranked!");
+  } catch (error) {
+    res.json("Error setting rank: " + error.message);
+  }
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
